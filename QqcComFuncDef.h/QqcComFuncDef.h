1faff9681f0fa8ddc42a1cfdb31ebf8363b02564
qqc <qqc@admindeiMac.local>
2017-05-14 17:07:29 +0800

    完善

---
+++ QqcComFuncDef.h/QqcComFuncDef.h
@@ -0,0 +1,69 @@
+//
+//  QqcComFuncDef.h
+//  QqcBaseFramework
+//
+//  Created by hc on 15/2/4.
+//  Copyright (c) 2015年 Qqc. All rights reserved.
+//
+
+#ifndef QqcBaseFramework_QqcComFuncDef_h
+#define QqcBaseFramework_QqcComFuncDef_h
+
+//获取医随身服务器其它大小图片
+#define img_with_size(_origin_, _size_) [((NSString*)_origin_) stringByReplacingOccurrencesOfString:@"default" withString:_size_]
+
+//字符串空值判断
+#define str_is_exist_qqc(_str_)     (_str_ && ![_str_ isEqualToString:@""])
+
+//NSInteger 2 NSString
+#define str_from_int(_int_)  [NSString stringWithFormat:@"%ld", (long)_int_]
+
+//把字符串连接转换成NSURL对象
+#define url_from_str(_str_) [NSURL URLWithString:_str_]
+
+//类名称
+#define name_class_qqc NSStringFromClass([self class])
+
+//App应用名称
+#define name_diplay_app_qqc [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"]
+
+//判断是否为iphone4
+#define is_iphone4_qqc() (fabs((double)[[UIScreen mainScreen] bounds].size.height-(double)480.f ) < DBL_EPSILON )
+
+//判断是否为iphone5
+#define is_iphone5_qqc() (fabs((double)[[UIScreen mainScreen] bounds].size.height-(double)568.f ) < DBL_EPSILON )
+
+//判断是否为iphone6
+#define is_iphone6_qqc() (fabs((double)[[UIScreen mainScreen] bounds].size.height-(double)667.f ) < DBL_EPSILON )
+
+//判断是否为iphone6p
+#define is_iphone6p_qqc() (fabs((double)[[UIScreen mainScreen] bounds].size.height-(double)736.f ) < DBL_EPSILON )
+
+//判断是否为ipod
+#define is_ipod_qqc() ([[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+
+//判断是否为ipad
+#define is_ipad_qqc() (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+
+//获取的一个cell 类的标识
+#define identity_of_cell(_strCellClassName_) [NSString stringWithFormat:@"%@Identity", _strCellClassName_]
+
+//去掉日期中的时间部分
+#define date_2_no_time(_date_) [[NSDate dateWithString:_date_ formatString:@"yyyy-MM-dd HH:mm:ss"] convertToStringWithFormat:@"yyyy-MM-dd"]
+
+//去掉日期中的时间秒部分
+#define date_2_no_second(_date_) [[NSDate dateWithString:_date_ formatString:@"yyyy-MM-dd HH:mm:ss"] convertToStringWithFormat:@"yyyy-MM-dd HH:mm"]
+
+//去掉时间中的秒部分
+#define time_2_no_second(_date_) [[NSDate dateWithString:_date_ formatString:@"HH:mm:ss"] convertToStringWithFormat:@"HH:mm"]
+
+//从日期中获取年份
+#define year_from_date(_date_) [[NSDate dateWithString:_date_ formatString:@"yyyy-MM-dd HH:mm:ss"] convertToStringWithFormat:@"yyyy"];
+
+//从日期中获取月份
+#define month_from_date(_date_) [[NSDate dateWithString:_date_ formatString:@"yyyy-MM-dd HH:mm:ss"] convertToStringWithFormat:@"MM"];
+
+//从日期中获取日份
+#define day_from_date(_date_) [[NSDate dateWithString:_date_ formatString:@"yyyy-MM-dd HH:mm:ss"] convertToStringWithFormat:@"dd"]
+
+#endif
